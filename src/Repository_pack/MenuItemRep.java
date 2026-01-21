965a603e8c1120264a9b0b289eac6cd7ae0f274b
temirlan-fr <bolattimok197227@gmail.com>
2026-01-21 13:37:28 +0500

    Temirlan: menu repository

---
+++ src/Repository_pack/MenuItemRep.java
@@ -0,0 +1,29 @@
+package Repository_pack;
+
+import Entity_pack.MenuItem;
+import java.sql.*;
+
+public class MenuItemRep {
+
+    public MenuItem findById(int id) {
+        try (Connection con = DatabaseConnection.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "SELECT * FROM menu_items WHERE id = ?");
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new MenuItem(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getDouble("price"),
+                        rs.getBoolean("available")
+                );
+            }
+            return null;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
