c70e5744f7dc08d714808c5fa67ffb32d8b6df44
Nurbolsyn <nurbolsyna2@gmail.com>
2026-01-22 15:56:54 +0500

    nurbolsyn Menuservice

--- src/Service_pack/OrderService.java
+++ src/Service_pack/OrderService.java
@@ -1,9 +1,13 @@
 package Service_pack;
 
 import Entity_pack.MenuItem;
+import Entity_pack.OrderItem;
 import Exception_pack.InvalidQuantity;
+import Exception_pack.OrderNotFound;
 import Repository_pack.OrderItemRep;
 import Repository_pack.OrderRep;
+
+import java.util.List;
 
 public class OrderService {
 
@@ -13,19 +17,30 @@ public class OrderService {
 
     public int placeOrder(int customerId, int menuItemId, int quantity) {
 
-        if (quantity <= 0) {
-            throw new InvalidQuantity("Quantity must be greater than 0");
+        if (quantity <= 0){ throw new InvalidQuantity("Quantity must be > 0");
         }
 
         MenuItem item = menuService.getAvailableMenuItem(menuItemId);
 
         int orderId = orderRepo.createOrder(customerId);
-        itemRepo.addItem(orderId, item.getId(), quantity);
+        if (orderId==-1)throw new OrderNotFound("Order creation failed");
 
+        itemRepo.addItem(orderId, menuItemId, quantity);
         return orderId;
     }
 
     public void completeOrder(int orderId) {
-        orderRepo.markCompleted(orderId);
+        boolean updated=orderRepo.markCompleted(orderId);
+        if(!updated)throw new OrderNotFound("Order not found!");
+
+    }
+    public double calculateTotalAmount(int orderId){
+    List<OrderItem>items=itemRepo.findByOrderId(orderId);
+    double total=0;
+    for(OrderItem item:items) {
+        MenuItem menuItem =menuService.getAvailableMenuItem(item.getMenuItemId());
+        total += menuItem.getPrice()*item.getQuantity();
+    }
+    return total;
     }
 }
