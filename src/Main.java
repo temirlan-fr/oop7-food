5ad74fab64d41f357184a348e869344f7ac7659e
temirlan-fr <bolattimok197227@gmail.com>
2026-01-22 14:57:27 +0500

    Temirlan main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,16 @@
-//import Repository_pack.DatabaseConnection;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Connecting to Supabase...");
-//        try (Connection connection = DatabaseConnection.getConnection()) {
-//            System.out.println("Connected successfully!");
-//            String sql = "SELECT CURRENT_TIMESTAMP";
-//            try (PreparedStatement stmt = connection.prepareStatement(sql);
-//                 ResultSet rs = stmt.executeQuery()) {
-//                if (rs.next()) {
-//                    System.out.println("Database time: " + rs.getTimestamp(1));
-//                }
-//            }
-//        } catch (SQLException e) {
-//            System.out.println("Error while connecting to database:");
-//            e.printStackTrace();
-//        }
-//    }
-//}
+import Service_pack.OrderService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        OrderService orderService = new OrderService();
+
+        // Заказ беру
+        int orderId = orderService.placeOrder(1, 1, 2);
+        System.out.println("Order created with ID: " + orderId);
+
+        // Заказ толықтау
+        orderService.completeOrder(orderId);
+        System.out.println("Order completed");
+    }
+}
