a71a76ae806711f224f41ffe80407604a8cf9021
temirlan-fr <bolattimok197227@gmail.com>
2026-01-22 17:30:51 +0500

    Temirlan menuitem

--- src/Entity_pack/MenuItem.java
+++ src/Entity_pack/MenuItem.java
@@ -6,6 +6,10 @@ public class MenuItem {
     private double price;
     private boolean available;
 
+    public MenuItem(){
+
+    }
+
     public MenuItem(int id, String name, double price, boolean available) {
         this.id = id;
         this.name = name;
@@ -25,4 +29,27 @@ public class MenuItem {
     public boolean isAvailable() {
         return available;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String toString() {
+        return "MenuItem{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", available=" + available +
+                '}';
+    }
+
 }
