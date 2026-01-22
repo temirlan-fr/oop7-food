a72ca8b10d61d4c66781ed13eedfc83875629e08
Nurbolsyn <nurbolsyna2@gmail.com>
2026-01-22 14:38:49 +0500

    nurbolsyn Menuservice

---
+++ src/Service_pack/OrderService.java
@@ -0,0 +1,31 @@
+package Service_pack;
+
+import Entity_pack.MenuItem;
+import Exception_pack.InvalidQuantity;
+import Repository_pack.OrderItemRep;
+import Repository_pack.OrderRep;
+
+public class OrderService {
+
+    private final OrderRep orderRepo = new OrderRep();
+    private final OrderItemRep itemRepo = new OrderItemRep();
+    private final MenuService menuService = new MenuService();
+
+    public int placeOrder(int customerId, int menuItemId, int quantity) {
+
+        if (quantity <= 0) {
+            throw new InvalidQuantity("Quantity must be greater than 0");
+        }
+
+        MenuItem item = menuService.getAvailableMenuItem(menuItemId);
+
+        int orderId = orderRepo.createOrder(customerId);
+        itemRepo.addItem(orderId, item.getId(), quantity);
+
+        return orderId;
+    }
+
+    public void completeOrder(int orderId) {
+        orderRepo.markCompleted(orderId);
+    }
+}
---
+++ src/Service_pack/PaymentService.java
@@ -0,0 +1,7 @@
+package Service_pack;
+
+public class PaymentService {
+    public void pay(double amount) {
+        System.out.println("Payment successful: " + amount);
+    }
+}
