5184447fbbd9311f9193962228c98b1ea51bb75d
temirlan-fr <bolattimok197227@gmail.com>
2026-01-22 17:30:34 +0500

    Temirlan main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import Service_pack.OrderService;
+import Service_pack.PaymentService;
 
 public class Main {
     public static void main(String[] args) {
-
         OrderService orderService = new OrderService();
+        PaymentService paymentService = new PaymentService();
 
         int orderId = orderService.placeOrder(1, 1, 2);
-        System.out.println("Order created with ID: " + orderId);
+        System.out.println("Created orderId = " + orderId);
+
+        double totalAmount = orderService.calculateTotalAmount(orderId);
+        paymentService.pay(totalAmount);
 
         orderService.completeOrder(orderId);
-        System.out.println("Order completed");
+        System.out.println("Order completed successfully");
     }
 }
