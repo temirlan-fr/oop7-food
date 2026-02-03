aa191a660c89783706c3b1df9c92fa40c89eee90
temirlan-fr <bolattimok197227@gmail.com>
2026-02-03 14:58:05 +0500

    Temirlan fixing problems

--- src/Factory_pack/MenuItemFactory.java
+++ src/Factory_pack/MenuItemFactory.java
@@ -6,28 +6,28 @@ public class MenuItemFactory {
 
     public static MenuItem createMenuItem(String type, int id, String name, double price, boolean available) {
         switch (type.toLowerCase()) {
-            case "Burger":
+            case "burger":
                 return new MenuItem.Builder()
                         .setId(id)
                         .setName(name)
                         .setPrice(price)
                         .setAvailable(available)
                         .build();
-            case "Garnir":
+            case "garnir":
                 return new MenuItem.Builder()
                         .setId(id)
                         .setName(name)
                         .setPrice(price)
                         .setAvailable(available)
                         .build();
-            case "Drink":
+            case "drink":
                 return new MenuItem.Builder()
                         .setId(id)
                         .setName(name + " Drink")
                         .setPrice(price)
                         .setAvailable(available)
                         .build();
-            case "Dish":
+            case "dish":
                 return new MenuItem.Builder()
                         .setId(id)
                         .setName(name)
